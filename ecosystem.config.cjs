eaf080be9b5500d40949196c52a27b8873695e40
manuelExtra <hello@mxtechsolutions.com>
2025-06-10 20:33:49 +0100

    implement workflow modification

---
+++ ecosystem.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: `hq-main`,
+      script: 'npm',
+      args: 'run preview',
+      env: {
+        PM2_SERVE_PATH: './dist',
+        PM2_SERVE_PORT: 4173,
+        PM2_SERVE_SPA: 'true',
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
