bdd725c1cc1cd205b4bf14e2a004d0f9c483fd15
manuelExtra <hello@mxtechsolutions.com>
2025-06-10 22:55:57 +0100

    implement ecosystem config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
       env: {
         // PM2_SERVE_PATH: './dist',
         PORT: 3000,
-        PM2_SERVE_SPA: 'true',
+        // PM2_SERVE_SPA: 'true',
         NODE_ENV: 'production',
       },
     },
